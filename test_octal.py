46db0123aa5b854357a972dfc2f1ab6b1a8d2fa2
Prashantdubeyfran9coo <Vandanadubey7669@gmail.com>
2026-03-29 16:38:17 +0530

    Added octal arithmetic functions and updated tests

--- test_octal.py
+++ test_octal.py
@@ -1,5 +1,6 @@
 import unittest
 from octal import *
+from arithmetic import *
 
 class TestOctal(unittest.TestCase):
 
@@ -12,7 +13,29 @@ class TestOctal(unittest.TestCase):
         self.assertEqual(sevensComplement("O'001"), "O'776")
         self.assertEqual(eightsComplement("O'777"), "O'001")
         self.assertEqual(sevensComplement("O'10"), "O'60")
+
+    def test_add_octal(self):
+        self.assertEqual(add_octal("O'10", "O'7"), "O'17")
+        self.assertEqual(add_octal("O'1", "O'1"), "O'2")
+
+    def test_subtract_octal(self):
+        self.assertEqual(subtract_octal("O'10", "O'7"), "O'1")
+
+    def test_subtract_negative(self):
+        with self.assertRaises(ValueError):
+            subtract_octal("O'7", "O'10")
+
+    def test_multiply_octal(self):
+        self.assertEqual(multiply_octal("O'2", "O'3"), "O'6")
+
+    def test_divide_octal(self):
+        self.assertEqual(divide_octal("O'10", "O'2"), "O'4")
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide_octal("O'10", "O'0")
     
+
 
 if __name__ == "__main__":
     unittest.main()

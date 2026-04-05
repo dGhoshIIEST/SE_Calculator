cd878ba8f5065904b7dfae5ba9e65c974d0e6814
Joydip104 <PaulJoydip.work@gmail.com>
2026-04-05 19:55:36 +0530

    Fixed merge issue

--- test_hex.py
+++ test_hex.py
@@ -197,23 +197,34 @@ class TestHexCalculator(unittest.TestCase):
     # PRATYUSH 3 TESTS
     # ---------------------------------
     
-    def test_subtract(self):
-        self.assertEqual(
-            self.hex_calc.subtract("H'F'", "H'5'"),
-            "H'A'"
-        )
+    def test_subtract_all_cases(self):
+        # Basic subtract scenarios
+        self.assertEqual(self.hex_calc.subtract("H'F'", "H'5'"), "H'A'")
+        self.assertEqual(self.hex_calc.subtract("H'10'", "H'1'"), "H'F'")
+        self.assertEqual(self.hex_calc.subtract("H'2'", "H'5'"), "H'-3'")
 
-    def test_subtract_with_borrow(self):
-        self.assertEqual(
-            self.hex_calc.subtract("H'10'", "H'1'"),
-            "H'F'"
-        )
+        # Functional edge cases
+        self.assertEqual(self.hex_calc.subtract("H'0'", "H'0'"), "H'0'")
+        self.assertEqual(self.hex_calc.subtract("H'1A3F'", "H'1A3F'"), "H'0'")
+        self.assertEqual(self.hex_calc.subtract("H'1000'", "H'1'"), "H'FFF'")
+        self.assertEqual(self.hex_calc.subtract("H'00A'", "H'0001'"), "H'9'")
+        self.assertEqual(self.hex_calc.subtract("H'a'", "H'3'"), "H'7'")
+        self.assertEqual(self.hex_calc.subtract("H'ABCDEF'", "H'123456'"), "H'999999'")
+        self.assertEqual(self.hex_calc.subtract("H'1'", "H'A'"), "H'-9'")
+        self.assertEqual(self.hex_calc.subtract("H'10'", "H'1F'"), "H'-F'")
 
-    def test_subtract_negative_result(self):
-        self.assertEqual(
-            self.hex_calc.subtract("H'2'", "H'5'"),
-            "H'-3'"
-        )
+        # Validation and input-type corner cases
+        with self.assertRaises(ValueError):
+            self.hex_calc.subtract("H'1G'", "H'1'")
+
+        with self.assertRaises(ValueError):
+            self.hex_calc.subtract("H'1'", "1")
+
+        with self.assertRaises(ValueError):
+            self.hex_calc.subtract(10, "H'1'")
+
+        with self.assertRaises(ValueError):
+            self.hex_calc.subtract("H'1'", None)
 
     # ---------------------------------
     # SNEHA 4 TESTS
